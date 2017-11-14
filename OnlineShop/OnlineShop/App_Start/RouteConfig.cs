2ad0fd42f0f185dea14fde298c30332019d62810
MinhTaii <96Phamminhtai@gmail.com>
2017-11-14 21:36:14 +0700

    origin

--- OnlineShop/OnlineShop/App_Start/RouteConfig.cs
+++ OnlineShop/OnlineShop/App_Start/RouteConfig.cs
@@ -96,7 +96,7 @@ namespace OnlineShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "OnlineShop.Controllers" }
 
             );
